9fa5e7412d7589ea5ef7b6b66ea4a92d25561cb4
Aleksandra <saenkaaa.riki@gmail.com>
2021-11-11 19:58:48 -0200

    Add to method plus and minus, add to class Count

--- FinanceAnalytic/MainWindow.xaml.cs
+++ FinanceAnalytic/MainWindow.xaml.cs
@@ -20,9 +20,51 @@ namespace FinanceAnalytic
     /// </summary>
     public partial class MainWindow : Window
     {
+        //private Window _window1;
+        //private Window _window2;
+        Count count = new Count(12000, "Семейный");
         public MainWindow()
         {
+            
+
             InitializeComponent();
+
+            textBlock.Text = Convert.ToString(count.Sum);
+            
+        }
+
+        private void button_Plus_Click(object sender, RoutedEventArgs e)
+        {
+            
+            count.PlusToSum(Convert.ToDouble(textBox.Text));
+            textBlock.Text = Convert.ToString(count.Sum);
+            //frame.Navigate(new Page1());
+        }
+
+        private void frame_Navigated(object sender, NavigationEventArgs e)
+        {
+            
+        }
+
+        
+
+        private void button_Minus_Click(object sender, RoutedEventArgs e)
+        {
+            count.MinusToSum(Convert.ToDouble(textBox.Text));
+            
+            textBlock.Text = Convert.ToString(count.Sum);
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox.Text, "[^0-9,]"))
+            {
+                MessageBox.Show("Пожалуйста, вводите только числа");
+                textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1);
+            }
+            
+            
+            //return value;
         }
     }
 }
