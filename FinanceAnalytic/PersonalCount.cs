904a10704112102216aaa7eb60d57c4ed00d4b66
Aleksandra <saenkaaa.riki@gmail.com>
2021-11-16 22:15:09 -0200

    Fixed constructor PersonalCount and added test for metod AddTransaction

--- FinanceAnalytic/PersonalCount.cs
+++ FinanceAnalytic/PersonalCount.cs
@@ -1,25 +1,28 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FinanceAnalytic
 {
-    class PersonalCount : Counts
+    public class PersonalCount : Counts
     {
-        public PersonalCount(double v1, string v2, int v3, Expense startCount)
+        public PersonalCount(double v1, string v2,  Expense startCount)
         {
-            Sum = v1;
+            Sum = v1 + startCount.Sum;
             Name = v2;
-            Transaction = new List<Transactions>((IEnumerable<Transactions>)startCount);
+            Transaction = new List<Transactions>();
+            Transaction.Add(startCount);               
         }
 
-        public double Sum { get; set; }
         public string Name { get; set; }
         public int Type { get; set; }
+        public double Sum { get; set; }
         public List<Transactions> Transaction { get; set; }
        
         public void AddTransaction(Transactions IncreaseOrExpense)
         {
+            //Transaction.Add()
             Transaction.Add(IncreaseOrExpense);
             Sum += IncreaseOrExpense.Sum;
         }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using FinanceAnalytic;
+using System;
 
 namespace Tests
 {
@@ -9,10 +11,41 @@ namespace Tests
         {
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(1200, 14, 2000, 6, 13214)]
+        [TestCase(0, 0, 0, 2, 0)]
+        [TestCase(0, 14, -2000, 0, 14)]
+        [TestCase(58, -14, 2, 1, 46)]
+
+        public void AddTransactionTest(double count, double sum, double transaction, int length, double expected)
         {
-            Assert.Pass();
+            //act
+            Expense expense = new Expense(sum, 0, DateTime.Today);
+            PersonalCount personalCount = new PersonalCount(count, "zhopa", expense);
+            for (int i = 0; i < length; i++)
+            {
+                personalCount.AddTransaction(new Expense(transaction, 0, DateTime.Today));
+            }
+            //assert
+            Assert.AreEqual(personalCount.Sum, expected);
+        }
+        
+        [TestCase (1200, 14, 2000, 6, 7)]
+        [TestCase (0, 0, 0, 2,3)]
+        [TestCase (0, 14, -2000, 0,1)]
+        [TestCase (58, -14, 2, 1,2)]
+        
+        public void AddTransactionVersionTwoTest(double count, double sum, double transaction, int length, int expected)
+        {
+            //act
+            Expense expense = new Expense(sum, 0, DateTime.Today);
+            PersonalCount personalCount = new PersonalCount(count, "zhopa", expense);
+            for (int i = 0; i < length; i++)
+            {
+                personalCount.AddTransaction(new Expense(transaction, 0, DateTime.Today));
+            }
+            
+            //assert
+            Assert.AreEqual(personalCount.Transaction.Count, expected);
         }
     }
 }
