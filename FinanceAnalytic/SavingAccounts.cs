763ee7af6c7ef58d08d8c7c849a9b296f304371b
Alla <suharevaalla88@gmail.com>
2021-11-18 16:14:52 +0300

    test

---
+++ FinanceAnalytic/SavingAccounts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceAnalytic
+{
+    class SavingAccounts : Counts
+    {
+        public double Sum { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void TransferBetweenCounts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
